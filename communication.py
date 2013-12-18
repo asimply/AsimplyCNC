2877a142c9b76d4148cf623593026d7c95328aa0
asimply <asimply@users.noreply.github.com>
2013-12-18 18:21:05 +0100

    Update communication.py

--- communication.py
+++ communication.py
@@ -7,10 +7,11 @@ import os
 import threading
 
 class order (object):
-    __init__(self, incrX, incrY, incrZ):
+    
+    def __init__(self, incrX, incrY, incrZ):
         self.incrX = incrX
         self.incrY = incrY
         self.incrZ = incrZ
         
-    __str__(self):
-        return ("M1X" + str(self.incrX) + "Y" + str(self.incrY) + "Z" + str(self.incrZ) + "\n")
+    def __str__(self):
+        return ("M1X" + str(self.incrX) + "Y" + str(self.incrY) + "Z" + str(self.incrZ) + "U" + str(self.velX) + "V" + str(self.velY) + "W" + str(self.velZ) + "\n")
